2fabcf2424b09281a34c6e72ac947af95c7372c4
lucas.amorim <lucas.amorim@nubank.com.br>
2021-03-31 20:54:02 -0300

    Decode for input 10

--- src/isa_mips/controllers/r_ops.clj
+++ src/isa_mips/controllers/r_ops.clj
@@ -51,13 +51,17 @@
         result      (bit-shift-right (a.number-base/bin->numeric rt-bin) shamt-value)]
     (db.memory/write-value! rd-addr (a.number-base/binary-string-signal-extend result 32))))
 
+(s/defn r-jump-and-link!
+  [rd :- s/Str rs :- s/Str rt :- s/Str shamt :- s/Str])
+
 (s/def r-table
   {"100000" {:str "add" :action add!}
    "100001" {:str "addu" :action addu! :unsigned true}
    "101010" {:str "slt" :action set-less-than!}
-   "001000" {:str "jr" :action jump-register! :jump-instruction true}
+   "001000" {:str "jr" :action jump-register! :hide-destiny-reg true :hide-second-reg true}
    "000000" {:str "sll" :action shift-left! :shamt true}
-   "000010" {:str "srl" :action shift-right! :shamt true}})
+   "000010" {:str "srl" :action shift-right! :shamt true}
+   "001001" {:str "jalr" :action r-jump-and-link! :hide-second-reg true}})
 
 (s/defn operation-str! :- s/Str
   [func :- s/Str
@@ -67,13 +71,12 @@
    shamt :- s/Str]
   (let [operation         (get r-table func)
         func-name         (:str operation)
-        _assert           (assert (not (nil? operation)) "Operation not found on r-table")
-        jump-instruction? (:jump-instruction operation)
+        _assert           (assert (not (nil? operation)) (str "Operation not found on r-table\n func: " func))
         shamt?            (:shamt operation)
-        destiny-reg-name  (when-not jump-instruction? (db.memory/read-name! (a.number-base/bin->numeric destiny-reg)))
+        destiny-reg-name  (when-not (:hide-destiny-reg operation) (db.memory/read-name! (a.number-base/bin->numeric destiny-reg)))
         first-reg-name    (when-not shamt? (db.memory/read-name! (a.number-base/bin->numeric first-reg)))
         shamt             (when shamt? (a.number-base/bin->numeric shamt))
-        second-name       (when-not jump-instruction?
+        second-name       (when-not (:hide-second-reg operation)
                             (db.memory/read-name! (a.number-base/bin->numeric second-reg)))]
     (str func-name " " (string/join ", " (remove nil? [destiny-reg-name first-reg-name second-name shamt])))))
 
