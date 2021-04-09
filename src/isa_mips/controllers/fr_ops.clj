0aedba8c6843faa759e4034ea704b3b9303ef640
lucas.amorim <lucas.amorim@nubank.com.br>
2021-04-09 00:54:42 -0300

    WIP

--- src/isa_mips/controllers/fr_ops.clj
+++ src/isa_mips/controllers/fr_ops.clj
@@ -3,15 +3,35 @@
             [isa-mips.db.coproc1 :as db.coproc1]
             [isa-mips.adapters.number-base :as a.number-base]
             [isa-mips.db.memory :as db.memory]
-            [clojure.string :as string]))
+            [clojure.string :as string]
+            [isa-mips.logic.binary :as l.binary]))
 
 (s/defn ^:private format-extension
   [fmt]
   (let [fmt-value (a.number-base/bin->numeric fmt)]
     (cond
       (= fmt-value 17) :d
-      (>= fmt-value 16) :s
+      (>= fmt-value 16) :w
       :else (throw (ex-info "Invalid format" {:format fmt})))))
+
+(s/defn ^:private move-float-to-reg!
+  [_ :- s/Str
+   reg :- s/Str
+   regular-reg :- s/Str
+   _]
+  (let [destiny-addr (a.number-base/bin->numeric regular-reg)
+        reg-value    (db.coproc1/read-value! (a.number-base/bin->numeric reg))]
+    (db.memory/write-value! destiny-addr reg-value)))
+
+(s/defn ^:private move-word-to-float!
+  [_ :- s/Str
+   reg :- s/Str
+   regular-reg :- s/Str
+   _]
+  (let [read-addr    (a.number-base/bin->numeric regular-reg)
+        destiny-addr (a.number-base/bin->numeric reg)
+        reg-value    (db.memory/read-reg-value! read-addr)]
+    (db.coproc1/write-value! destiny-addr reg-value)))
 
 (s/defn ^:private double-move!
   [destiny-reg :- s/Str
@@ -33,47 +53,39 @@
         reg-value    (db.coproc1/read-value! reg-num)]
     (db.coproc1/write-value! destiny-addr reg-value)))
 
-(s/defn ^:private move-float-to-reg!
-  [_ :- s/Str
-   reg :- s/Str
-   regular-reg :- s/Str
-   _]
-  (let [destiny-addr (a.number-base/bin->numeric regular-reg)
-        reg-value    (db.coproc1/read-value! (a.number-base/bin->numeric reg))]
-    (db.memory/write-value! destiny-addr reg-value)))
-
-(s/defn ^:private move-word-to-float!
-  [_ :- s/Str
-   reg :- s/Str
-   regular-reg :- s/Str
-   _]
-  (let [read-addr    (a.number-base/bin->numeric regular-reg)
-        destiny-addr (a.number-base/bin->numeric reg)
-        reg-value    (db.memory/read-reg-value! read-addr)]
-    (db.coproc1/write-value! destiny-addr reg-value)))
-
 (s/defn ^:private mov!
   [destiny-reg :- s/Str
    reg :- s/Str
    regular-reg :- s/Str
    fmt :- s/Str]
   (condp = (format-extension fmt)
-    :s (floating-point-move! destiny-reg reg regular-reg)
+    :w (floating-point-move! destiny-reg reg regular-reg)
     :d (double-move! destiny-reg regular-reg regular-reg)))
 
-(s/defn ^:private cvt.d
+(s/defn ^:private convert-integer-to-double!
+  [destiny-reg :- s/Str
+   reg :- s/Str
+   _]
+  (let [destiny-addr (a.number-base/bin->numeric destiny-reg)
+        reg-bin      (db.coproc1/read-value! (a.number-base/bin->numeric reg))
+        result       (double (a.number-base/bin->numeric reg-bin))
+        result-bin   (l.binary/signal-extend-nbits (a.number-base/double->bin result) 64)]
+    (db.coproc1/write-value! (+ destiny-addr 1) (subs result-bin 0 32))
+    (db.coproc1/write-value! destiny-addr (subs result-bin 32 64))))
+
+(s/defn ^:private cvt-d!
   [destiny-reg :- s/Str
    reg :- s/Str
    regular-reg :- s/Str
    fmt :- s/Str]
   (condp = (format-extension fmt)
-    :s
-    :d (throw)))
+    :w (convert-integer-to-double! destiny-reg reg regular-reg)
+    :d (throw (ex-info "Not implemented yet!" {:func "cvt.d"}))))
 
 (s/def ^:private fr-table-by-func
   {"000110" {:str "mov" :action mov!}
-   "100001" {:str "cvt.d" :action (constantly nil)}
-   "000000" {:str "add"  :action (constantly nil)}
+   "100001" {:str "cvt.d" :action cvt-d!}
+   "000000" {:str "add" :action (constantly nil)}
    "000011" {:str "div" :action (constantly nil)}
    "100000" {:str "cvt.s" :action (constantly nil)}
    "000010" {:str "mul" :action (constantly nil)}})
